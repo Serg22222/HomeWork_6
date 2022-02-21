0ef637c42d018d84d13f74dac6f1fd7b68f77c22
selischev_sb <ssb22@mail.ru>
2022-02-21 03:16:03 +0300

    final commit

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -10,6 +10,7 @@ public class MainTest {
 
     public final int SIZE = 10;
     IndexStorage indexStorage = new IndexStorage(SIZE);
+    ArrayIndexStorage arrayIndexStorage = new ArrayIndexStorage(new int[]{1, 2, 3, 4, 5, 6, 7});
 
     @Test
     public void indexStorgeOutOfBoundsTest() {
@@ -17,6 +18,9 @@ public class MainTest {
         assertThrows(
                 IndexOutOfBoundsException.class,
                 () -> indexStorage.get(10));
+        assertThrows(
+                IndexOutOfBoundsException.class,
+                () -> indexStorage.get(-1));
     }
 
     @Test
@@ -25,5 +29,22 @@ public class MainTest {
         int actual = indexStorage.get(index);
         int expected = index % 2 == 0 ? index : -index;
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setIndexStorageReverseTest(){
+        int [] expectedArr = new int[]{-9, 8, -7, 6, -5, 4, -3, 2, -1, 0};
+        assertEquals(Arrays.toString(expectedArr), Arrays.toString(indexStorage.reverse()));
+    }
+
+    @Test
+    public void getArrayIndexStorageTest(){
+        assertEquals(1, arrayIndexStorage.get(0));
+        assertEquals(6, arrayIndexStorage.get(5));
+    }
+
+    @Test
+    public void sizeArrayIndexStorageTest(){
+        assertEquals(Arrays.toString(new int[]{1, 2, 3, 4, 5, 6, 7}), Arrays.toString(arrayIndexStorage.size()));
     }
 }
